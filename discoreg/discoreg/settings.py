17adbec6bbd8733a2f65c6576103ec55984eb39b
Joe Cabrera <calcmaster16@gmail.com>
2023-10-02 21:55:58 -0400

    adding dj_database_url for portability

--- discoreg/discoreg/settings.py
+++ discoreg/discoreg/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -91,10 +92,9 @@ WSGI_APPLICATION = "discoreg.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///{}".format(os.path.join(BASE_DIR, 'db.sqlite3')),
+    ),
 }
 
 
